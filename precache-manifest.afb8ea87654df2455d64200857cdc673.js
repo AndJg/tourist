3937ac0f54525c346d8a7b09fdec4ca511c00d72
Andrzej <eviljajo@gmail.com>
2020-04-08 11:03:43 +0200

    Updates

---
+++ precache-manifest.afb8ea87654df2455d64200857cdc673.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd535b434c1db7e102d14f4669dba0d4",
+    "url": "/tourist/index.html"
+  },
+  {
+    "revision": "edad14d6374dbefa3daa",
+    "url": "/tourist/static/css/main.4a163f5f.chunk.css"
+  },
+  {
+    "revision": "ddbf04c638a329faa111",
+    "url": "/tourist/static/js/2.5f808e91.chunk.js"
+  },
+  {
+    "revision": "a5288a4ed4e4beba8413aa5c9ae969ea",
+    "url": "/tourist/static/js/2.5f808e91.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "edad14d6374dbefa3daa",
+    "url": "/tourist/static/js/main.0c60c63d.chunk.js"
+  },
+  {
+    "revision": "94b0031dc2eb3d432caf",
+    "url": "/tourist/static/js/runtime-main.895f2d5e.js"
+  },
+  {
+    "revision": "96018a9504b2a3db64e747e3d98a4bd5",
+    "url": "/tourist/static/media/1.96018a95.png"
+  },
+  {
+    "revision": "935f874658ab53fcb6f274b247b8de22",
+    "url": "/tourist/static/media/2.935f8746.png"
+  },
+  {
+    "revision": "892387e9c258824b9a8936b8dd919885",
+    "url": "/tourist/static/media/3.892387e9.png"
+  },
+  {
+    "revision": "8ec252a733e7b17a00dbee72ae407beb",
+    "url": "/tourist/static/media/4.8ec252a7.png"
+  },
+  {
+    "revision": "abccb58547503ac22fe9c6adc4c4b6f4",
+    "url": "/tourist/static/media/5.abccb585.png"
+  },
+  {
+    "revision": "d8c01f4aeb74dba1e7ee230ba51753d2",
+    "url": "/tourist/static/media/6.d8c01f4a.png"
+  },
+  {
+    "revision": "0acbc000f5130d1b2b1f9a1215505822",
+    "url": "/tourist/static/media/BG.0acbc000.png"
+  },
+  {
+    "revision": "2c51cae9f15a9979109fc4d547204eae",
+    "url": "/tourist/static/media/BG2.2c51cae9.jpg"
+  },
+  {
+    "revision": "805cf3755ec339161eab7dc8cdb77631",
+    "url": "/tourist/static/media/BG3.805cf375.jpeg"
+  },
+  {
+    "revision": "5b5e2b2f8f79e8b0dc2d281f07a006e3",
+    "url": "/tourist/static/media/BG4.5b5e2b2f.jpeg"
+  },
+  {
+    "revision": "d9aa1b1c38ccda286cda98bce4cf18bd",
+    "url": "/tourist/static/media/Indonesia.d9aa1b1c.png"
+  },
+  {
+    "revision": "61bf5e3d0f3112ca9397b0fb594618ac",
+    "url": "/tourist/static/media/Italy.61bf5e3d.png"
+  },
+  {
+    "revision": "8723e5b67a03c8f124eae558cdbc0c34",
+    "url": "/tourist/static/media/Singapore.8723e5b6.png"
+  },
+  {
+    "revision": "8723e5b67a03c8f124eae558cdbc0c34",
+    "url": "/tourist/static/media/Thailand.8723e5b6.png"
+  },
+  {
+    "revision": "270842913deccabea1ff70701fb22e91",
+    "url": "/tourist/static/media/bg.27084291.png"
+  },
+  {
+    "revision": "45982fb28fd0eb8595106e51ef654d00",
+    "url": "/tourist/static/media/bgOff.45982fb2.png"
+  },
+  {
+    "revision": "c708c0eda93af49313930330d5770ab2",
+    "url": "/tourist/static/media/offer1.c708c0ed.png"
+  },
+  {
+    "revision": "7dc38cfd0d79e8ddcaa8239ef2507289",
+    "url": "/tourist/static/media/offer2.7dc38cfd.png"
+  },
+  {
+    "revision": "8e215535279e7d848c32336b625a2eae",
+    "url": "/tourist/static/media/offer3.8e215535.png"
+  },
+  {
+    "revision": "d8400d8cef704e61c942204a328fa76b",
+    "url": "/tourist/static/media/testi1.d8400d8c.png"
+  },
+  {
+    "revision": "4f53666d0e803ca2eaa3b805ae1ae834",
+    "url": "/tourist/static/media/testi2.4f53666d.png"
+  },
+  {
+    "revision": "94f70bf3c71dbbea7604b6d3ad27ec09",
+    "url": "/tourist/static/media/testi3.94f70bf3.png"
+  }
+]);
